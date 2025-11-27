44b3d96c22b6681bd4641d3e786b8b20f8bb558c
Cüneyt Kaya <kayacuneyd@gmail.com>
2025-11-27 19:49:25 +0100

    refactor: Display domain from local variable instead of order store.

--- frontend/src/routes/checkout/+page.svelte
+++ frontend/src/routes/checkout/+page.svelte
@@ -55,7 +55,7 @@
           <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt class="text-sm font-medium text-gray-500">Domain</dt>
             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 font-bold text-blue-600">
-              {$orderStore.domain || 'Seçilmedi'}
+              {domain || 'Seçilmedi'}
             </dd>
           </div>
           <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
